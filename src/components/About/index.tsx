7ab0a15d4e86f55c8a532ca6b2e05f86ed700621
¡Duque <atanaelleonardo@gmail.com>
2025-01-04 00:02:24 -0300

    feat : create new Route <About />

---
+++ src/components/About/index.tsx
@@ -0,0 +1,5 @@
+import './styles.css';
+
+export const About = () => {
+  return <div>About</div>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/global.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { About } from './components/About/index.tsx';
 import { Home } from './components/Home/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
