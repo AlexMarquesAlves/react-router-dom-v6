7e3422f144e78656626d81436dd577bade031c41
¡Duque <atanaelleonardo@gmail.com>
2025-01-04 00:20:55 -0300

    `Update CSS imports to use 'style.css' instead of 'styles.css' in About and Home components`

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,4 +1,4 @@
-import './styles.css';
+import './style.css';
 
 export const About = () => {
   return <div>About</div>;
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import './styles/global.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { About } from './components/About/index.tsx';
 import { Home } from './components/Home/index.tsx';
+import { Menu } from './components/Menu/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
