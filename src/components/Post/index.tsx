c8b0dfa8282d18e104b3bb8b518c9efb0e916447
¡Duque <atanaelleonardo@gmail.com>
2025-01-04 06:37:01 -0300

    feat: `Update Post component with new content and styles`

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -3,7 +3,16 @@ import './style.css';
 export const Post = () => {
   return (
     <div>
-      <h1>Hello, World</h1>
+      <h1>Post</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+        earum quis fugit tenetur dolorem vero ad quia unde, veniam repellat
+        libero explicabo doloribus. Sint velit neque, dicta architecto nesciunt
+        quisquam? Laboriosam, sit. Atque maiores placeat, dolorum, voluptas
+        voluptatibus nesciunt ex, illum quibusdam eveniet quisquam natus
+        consequatur dolor necessitatibus voluptate iste molestiae saepe soluta?
+        Consequuntur nam sed deserunt dolor soluta. Perspiciatis.
+      </p>
     </div>
   );
 };
